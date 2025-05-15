be16316243fcefbdcbc7989dac4da80bf5d2477d
KBOlifant <241299@virtualwindow.co.za>
2025-05-15 14:39:35 +0200

    Mongo front and backend integration

---
+++ mern-backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+//const MONGO_URI = 'mongodb+srv://241299:<db_password>@cluster0.dxar4cf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+
+ app.get('/', (req, red) => {
+     res.send('Howdy');
+ });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
